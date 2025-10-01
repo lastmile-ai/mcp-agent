be6587738ccf3936f1a63930b451f437f79ee585
amitandale <amitandale2025@gmail.com>
2025-10-01 12:41:32 -0500

    Update test_loader_parse.py

--- tests/registry/test_loader_parse.py
+++ tests/registry/test_loader_parse.py
@@ -1,12 +1,11 @@
-import io
 import yaml
 from mcp_agent.registry.loader import load_tools_yaml
 
 def test_parse_tools_yaml_list():
     data = {"tools": [{"name":"x","base_url":"http://x:1","version":"1.0.0"}]}
-    buf = io.StringIO(yaml.safe_dump(data))
     # emulate reading from file by writing to a temp file
-    import tempfile, os, pathlib
+    import tempfile
+    import pathlib
     with tempfile.TemporaryDirectory() as d:
         p = pathlib.Path(d)/"tools.yaml"
         p.write_text(yaml.safe_dump(data))
